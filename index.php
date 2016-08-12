18c8397901695f67c91910dc3f3f6af5da7f82b9
Guilherme Martins <adm.gms@gmail.com>
2016-08-12 12:06:08 -0300

    .

---
+++ index.php
@@ -0,0 +1,117 @@
+
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drag</title>
+      <link href='css/teste2.css' rel='stylesheet' type='text/css'>
+      <link href='css/demo.css' rel='stylesheet' type='text/css'>
+      <link href='css/main.css' rel='stylesheet' type='text/css'>
+      <link href='css/reset.css' rel='stylesheet' type='text/css'>
+          <script src="js/parallax.js"></script>
+          <link rel="stylesheet" href="css/font-awesome.css">
+          <link rel="stylesheet" href="css/bulma.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+            <!-- Sortable.js -->
+            <script src="http://rubaxa.github.io/Sortable/Sortable.js"></script>
+            <script src="graf.js"></script>
+
+</head>
+<body>
+
+<section class="columns">
+<ul id="items">
+<li class="itm column">
+  <section class="small-widget">
+    <section class="loading-bar"></section>
+      <section class="icon">
+        <div class="loader-00">
+          <i class="fa fa-cog fa-spin fa-3x fa-fw" style="color: #5dd054"></i>
+          <span class="sr-only">Loading...</span>
+        </div>
+    </section>
+  </section>
+</li>
+<li class="itm column">
+  <section class="small-widget">
+    <section class="loading-bar"></section>
+      <section class="icon">
+        <div class="loader-00">
+          <i class="fa fa-cog fa-spin fa-3x fa-fw" style="color: #5dd054"></i>
+          <span class="sr-only">Loading...</span>
+        </div>
+    </section>
+  </section>
+</li>
+<li class="itm column">
+  <section class="small-widget">
+    <section class="loading-bar"></section>
+      <section class="icon">
+        <div class="loader-00">
+          <i class="fa fa-cog fa-spin fa-3x fa-fw" style="color: #5dd054"></i>
+          <span class="sr-only">Loading...</span>
+        </div>
+    </section>
+  </section>
+</li>
+<li class="itm column">
+  <section class="small-widget">
+    <section class="loading-bar"></section>
+      <section class="icon">
+        <div class="loader-00">
+          <i class="fa fa-cog fa-spin fa-3x fa-fw" style="color: #5dd054"></i>
+          <span class="sr-only">Loading...</span>
+        </div>
+    </section>
+  </section>
+</li>
+<li class="itm column">
+  <section class="medium-widget">
+    <section class="loading-bar"></section>
+      <section class="icon">
+        <div class="loader-00">
+          <i class="fa fa-cog fa-spin fa-3x fa-fw" style="color: #5dd054"></i>
+          <span class="sr-only">Loading...</span>
+        </div>
+    </section>
+  </section>
+</li>
+<li class="itm column">
+  <section class="medium-widget">
+    <section class="loading-bar"></section>
+      <section class="icon">
+        <div class="loader-00">
+          <i class="fa fa-cog fa-spin fa-3x fa-fw" style="color: #5dd054"></i>
+          <span class="sr-only">Loading...</span>
+        </div>
+    </section>
+  </section>
+</li>
+
+<li class="itm column">
+  <section class="large-widget">
+    <section class="loading-bar"></section>
+      <section class="icon">
+        <div class="loader-00">
+          <i class="fa fa-cog fa-spin fa-3x fa-fw" style="color: #5dd054"></i>
+          <span class="sr-only">Loading...</span>
+        </div>
+    </section>
+  </section>
+</li>
+
+
+
+
+</ul>
+</section>
+<script id="jsbin-javascript">
+Sortable.create(items, {
+  animation: 200
+});
+
+</script>
+	<script src="sortable.js">
+
+</body>
